276597702201c502b8f16a3868a381aa8fe6a7ed
Parthbamane <84703062+parthbamane@users.noreply.github.com>
2025-11-26 17:43:38 +0530

    Update axios.js

--- src/auth/axios.js
+++ src/auth/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 const instance = axios.create({
-  baseURL: 'http://localhost:4000',
+  baseURL: 'https://e-commerce-portal-xi.vercel.app',
   timeout: 5000
 })
 export default instance
